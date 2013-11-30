410ff3455cb4639f51e6db37446f63b1f0080391
Tristan Slominski <tristan.slominski@gmail.com>
2013-11-29 21:24:55 -0600

    Erlang challenge refactored for actor state in closure approach.

--- scripts/erlangChallenge.js
+++ scripts/erlangChallenge.js
@@ -44,73 +44,75 @@ var Tart = require('../index.js');
 
 var config = new Tart();
 
-var ringMemberBeh = function ringMemberBeh (event) {
-    if (event.message.name == 'build') {
-        if (event.message.counter > 0) {
-            event.data.next = event.sponsor.createValue(ringMemberBeh);
-            event.message.counter--;
-            event.sponsor.send(event.data.next, event.message);
+var ringMemberBeh = function () {
+    var next = null;
+    return function (msg) {
+        if (msg.name == 'build') {
+            if (msg.counter > 0) {
+                next = this.event.context.sponsor.create(ringMemberBeh());
+                msg.counter--;
+                next(msg);
+            } else {
+                next = msg.seed;
+                next('start');
+            }
         } else {
-            event.data.next = event.message.seed;
-            event.sponsor.send(event.data.next, 'start');
+            next(msg);
         }
-    } else {
-        // forward to next
-        event.sponsor.send(event.data.next, event.message);
-    }
+    };
 };
 
-var seed = config.createValue(function (event) {
-    if (event.message == 'build') {
-        event.data.next = event.sponsor.createValue(ringMemberBeh);
-        event.sponsor.send(event.data.next, {
-            name: 'build',
-            counter: M - 1,
-            seed: event.target
-        });
-    } else if (event.message == 'start') {
-        constructionEndTime = process.hrtime();
-        console.log('constructed ' + M + ' actor ring');
-        event.sponsor.send(event.data.next, N);
-    } else if (event.message > 0) {
-        loopCompletionTimes.push(process.hrtime());
-        // keep sending
-        process.stdout.write('.');
-        event.message--;
-        event.sponsor.send(event.data.next, event.message);
-    } else {
-        loopCompletionTimes.push(process.hrtime());
-        process.stdout.write('\ndone\n');
-        var constructionTime = [];
-        constructionTime[0] = constructionEndTime[0] - constructionStartTime[0];
-        constructionTime[1] = constructionEndTime[1] - constructionStartTime[1];
-        console.log('all times in NANOSECONDS');
-        console.log('construction time:');
-        console.log(constructionTime[0] * 1e9 + constructionTime[1]);
-        console.log('loop times:');
-        var loopIntervals = [];
-        var prevTime = constructionEndTime;
-        var counter = 0;
-        loopCompletionTimes.forEach(function(time) {
-            counter++;
-            var interval = [];
-            interval[0] = time[0] - prevTime[0];
-            interval[1] = time[1] - prevTime[1];
-            console.log(interval[0] * 1e9 + interval[1]);
-            loopIntervals.push(interval);
-            prevTime = time;
-        });
-        console.log('loop average:');
-        var avgInterval = [0, 0];
-        loopIntervals.forEach(function(interval) {
-            avgInterval[0] += interval[0];
-            avgInterval[1] += interval[1];
-        });
-        avgInterval[0] = avgInterval[0] / counter;
-        avgInterval[1] = avgInterval[1] / counter;
-        console.log(avgInterval[0] * 1e9 + avgInterval[1]);        
-    }
-});
+var seedBeh = function () {
+    var next = null;
+    return function (msg) {
+        if (msg == 'build') {
+            next = this.event.context.sponsor.create(ringMemberBeh());
+            next({name: 'build', counter: M - 1, seed: this});
+        } else if (msg == 'start') {
+            constructionEndTime = process.hrtime();
+            console.log('constructed ' + M + ' actor ring');
+            next(N);
+        } else if (msg > 0) {
+            loopCompletionTimes.push(process.hrtime());
+            // keep sending
+            process.stdout.write('.');
+            msg--;
+            next(msg);
+        } else {
+            loopCompletionTimes.push(process.hrtime());
+            process.stdout.write('\ndone\n');
+            var constructionTime = [];
+            constructionTime[0] = constructionEndTime[0] - constructionStartTime[0];
+            constructionTime[1] = constructionEndTime[1] - constructionStartTime[1];
+            console.log('all times in NANOSECONDS');
+            console.log('construction time:');
+            console.log(constructionTime[0] * 1e9 + constructionTime[1]);
+            console.log('loop times:');
+            var loopIntervals = [];
+            var prevTime = constructionEndTime;
+            var counter = 0;
+            loopCompletionTimes.forEach(function(time) {
+                counter++;
+                var interval = [];
+                interval[0] = time[0] - prevTime[0];
+                interval[1] = time[1] - prevTime[1];
+                console.log(interval[0] * 1e9 + interval[1]);
+                loopIntervals.push(interval);
+                prevTime = time;
+            });
+            console.log('loop average:');
+            var avgInterval = [0, 0];
+            loopIntervals.forEach(function(interval) {
+                avgInterval[0] += interval[0];
+                avgInterval[1] += interval[1];
+            });
+            avgInterval[0] = avgInterval[0] / counter;
+            avgInterval[1] = avgInterval[1] / counter;
+            console.log(avgInterval[0] * 1e9 + avgInterval[1]);        
+        }            
+    };
+};
 
 constructionStartTime = process.hrtime();
-config.send(seed, 'build');
+var seed = config.create(seedBeh());
+seed('build');
