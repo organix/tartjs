74427aabbe934ff176d0b891c011ffb7a40289f0
Dale Schumacher <dalnefre@yahoo.com>
2013-11-30 09:03:10 -0600

    Try to use BECOME (broken)

--- scripts/erlangChallenge.js
+++ scripts/erlangChallenge.js
@@ -44,6 +44,41 @@ var Tart = require('../index.js');
 
 var config = new Tart();
 
+var ring_link = function ring_link(next) {
+	return function ring_link_beh(n) {
+		next(n);
+	};
+};
+
+var ring_last = function ring_last(first) {
+	return function ring_last_beh(n) {
+        loopCompletionTimes.push(process.hrtime());
+		if (--n > 0) {
+            process.stdout.write('.');
+			first(n);
+		} else {
+			this.event.context.behavior = function sink_beh(msg) {};
+			reportProcessTimes();
+		}
+	};
+};
+
+var ring_builder = function ring_builder(m) {
+	return function ring_builder_beh(msg) {
+		process.stdout.write(' msg=' + JSON.stringify(msg) + ' m=' + m +'\n');
+		if (--m > 0) {
+			var next = this.event.context.sponsor.create(ring_builder(m));
+			next(msg);
+			this.event.context.behavior = rink_link(next);
+		} else {
+            constructionEndTime = process.hrtime();
+			process.stdout.write('sending ' + msg.n + ' messages\n');
+			msg.first(msg.n);
+			this.event.context.behavior = rink_last(msg.first);
+		}
+	};
+};
+
 var ringMemberBeh = function () {
     var next = null;
     return function (msg) {
@@ -80,39 +115,46 @@ var seedBeh = function () {
             next(msg);
         } else {
             loopCompletionTimes.push(process.hrtime());
-            process.stdout.write('\ndone\n');
-            var constructionTime = [];
-            constructionTime[0] = constructionEndTime[0] - constructionStartTime[0];
-            constructionTime[1] = constructionEndTime[1] - constructionStartTime[1];
-            console.log('all times in NANOSECONDS');
-            console.log('construction time:');
-            console.log(constructionTime[0] * 1e9 + constructionTime[1]);
-            console.log('loop times:');
-            var loopIntervals = [];
-            var prevTime = constructionEndTime;
-            var counter = 0;
-            loopCompletionTimes.forEach(function(time) {
-                counter++;
-                var interval = [];
-                interval[0] = time[0] - prevTime[0];
-                interval[1] = time[1] - prevTime[1];
-                console.log(interval[0] * 1e9 + interval[1]);
-                loopIntervals.push(interval);
-                prevTime = time;
-            });
-            console.log('loop average:');
-            var avgInterval = [0, 0];
-            loopIntervals.forEach(function(interval) {
-                avgInterval[0] += interval[0];
-                avgInterval[1] += interval[1];
-            });
-            avgInterval[0] = avgInterval[0] / counter;
-            avgInterval[1] = avgInterval[1] / counter;
-            console.log(avgInterval[0] * 1e9 + avgInterval[1]);        
+			reportProcessTimes();
         }            
     };
 };
 
+var reportProcessTimes = function () {
+	process.stdout.write('\ndone\n');
+	var constructionTime = [];
+	constructionTime[0] = constructionEndTime[0] - constructionStartTime[0];
+	constructionTime[1] = constructionEndTime[1] - constructionStartTime[1];
+	console.log('all times in NANOSECONDS');
+	console.log('construction time:');
+	console.log(constructionTime[0] * 1e9 + constructionTime[1]);
+	console.log('loop times:');
+	var loopIntervals = [];
+	var prevTime = constructionEndTime;
+	var counter = 0;
+	loopCompletionTimes.forEach(function(time) {
+		counter++;
+		var interval = [];
+		interval[0] = time[0] - prevTime[0];
+		interval[1] = time[1] - prevTime[1];
+		console.log(interval[0] * 1e9 + interval[1]);
+		loopIntervals.push(interval);
+		prevTime = time;
+	});
+	console.log('loop average:');
+	var avgInterval = [0, 0];
+	loopIntervals.forEach(function(interval) {
+		avgInterval[0] += interval[0];
+		avgInterval[1] += interval[1];
+	});
+	avgInterval[0] = avgInterval[0] / counter;
+	avgInterval[1] = avgInterval[1] / counter;
+	console.log(avgInterval[0] * 1e9 + avgInterval[1]);        
+};
+
+console.log('starting ' + M + ' actor ring');
 constructionStartTime = process.hrtime();
 var seed = config.create(seedBeh());
 seed('build');
+//var ring = config.create(ring_builder(M));
+//ring({ first:ring, n:N });
