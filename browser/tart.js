e84f6e03e2c3a3c28be1ecf582b27074c71736b2
Dale Schumacher <dalnefre@yahoo.com>
2014-03-01 15:08:30 -0600

    Extract tart.js and use update sequence numbers

---
+++ browser/tart.js
@@ -0,0 +1,31 @@
+/*
+ * TartJS -- Tiny Actor-RunTime
+ */
+/*global TART */
+var TART = (function () {
+    var tart = {};
+    tart.config = function config(options) {
+		options = options || {};
+		var dispatch = options.dispatch || function (fn) { setTimeout(fn, 0); };
+		var fail = options.fail || function (exception) {};
+		var sponsor = function create(behavior) {
+			var actor = function send(message) {
+				dispatch(function deliver() {
+					try {
+						context.behavior(message);
+					} catch (exception) {
+						fail(exception);
+					};
+				});
+			};
+			var context = {
+				self: actor,
+				behavior: behavior,
+				sponsor: sponsor
+			};
+			return actor;
+		};
+		return sponsor;
+	};
+    return tart;
+})();
